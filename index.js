56ef3e120688ff18fdcb3d385292d78036d7cc40
melissamclaughlin <melissamclaughlindesigns@gmail.com>
2016-03-01 17:30:12 -0500

    Modified Javascript

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+var city = $('#city-type').val();
+var city = ['NYC', 'LA', 'SF', 'ATX', 'SYD'];
 
-$(document).ready(function() {
-  $('#submit-btn').click(function(){ 
-  event.preventDefault();  
-    var city = $('#city-type').val();
-    $('#city-input').val(''); 
-       console.log(city);
-    if (city == "New York" || city =="NYC" || city == "New York City") { 
-      $('body').attr('class','nyc');
-    } else if (city == "San Francisco" || city == "SF" || city =="Bay Area") {      
-       $('body').attr('class','sf');
-    } else if (city == "Los Angeles" || city =="LA" || city == "LAX") {      
-       $('body').attr('class','la');
-    } else if (city == "Austin" || city =="ATX") {      
-       $('body').attr('class','austin');
-    } else if (city == "Sydney" || city =="SYD") {      
-       $('body').attr('class','sydney');
-    }
-  });
-});
+ for (var i = 0; i < city.length; i++ ) {
+    $('#city-options').append('<option>' + city[i] + '<option>');
+ }
+
+     $('form').on('change', '#options', function(){
+      console.log();
+      var city = $('#city-options').val()
+       if (city == "NYC" || city =="ny"|| city =="nyc" || city == "New York" || city == "New York City" || city == "new york city"){ // How do I do other options: ny, New york, etc?
+          $('body').attr('class','nyc');
+        } else if (city == "SF" || city == "sf" || city =="san fran" || city == "San Fran" || city == "San Francisco") {      
+           $('body').attr('class','sf');
+        } else if (city == "LA" || city =="la" || city =="los angeles" || city == "Los Angeles" || city == "LAX") {      
+           $('body').attr('class','la');
+        } else if (city == "AUS" || city =="austin" || city == "Austin" || city == "Austin Texas" || city == "ATX") {      
+           $('body').attr('class','austin');
+        } else if (city == "SYD" || city =="sydney" || city == "Sydney") {      
+           $('body').attr('class','sydney');
+       }
+     });
+
+
+
